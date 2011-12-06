e17bf3d6e10edbfa9d8c22af1db7472d2a115b0e
nbxx <SND\nbxx_cp>
2011-12-06 07:47:52 +0000

    add DevLib.ExtensionMethods

---
+++ main/product/codes/DevLib.ExtensionMethods/StringExtensions.cs
@@ -0,0 +1,50 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="StringExtensions.cs" company="Contoso Corporation">
+//     Copyright (c) Contoso Corporation. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace DevLib.ExtensionMethods
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    /// <summary>
+    /// String Extensions
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Convert Hex string to bytes
+        /// </summary>
+        /// <param name="data">Hex string</param>
+        /// <returns>bytes</returns>
+        public static byte[] ToHexByte(this string data)
+        {
+            string hexPattern = "^[0-9a-fA-F]+$";
+
+            string temp = data.Trim(new char[] { ' ', '\n', '\r' });
+
+            if (Regex.IsMatch(temp, hexPattern))
+            {
+                if (data.Length % 2 == 1)
+                {
+                    temp = "0" + temp;
+                }
+
+                int length = temp.Length / 2;
+                byte[] result = new byte[length];
+
+                for (int i = 0; i < length; i++)
+                {
+                    result[i] = Convert.ToByte(temp.Substring(i * 2, 2), 16);
+                }
+
+                return result;
+            }
+            else
+            {
+                throw new ArgumentException(string.Format("\"{0}\" is not a Hex String.", data));
+            }
+        }
+    }
+}

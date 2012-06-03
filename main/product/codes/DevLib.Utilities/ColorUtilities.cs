eb46f8ed9f9aaede1dd89911e576d88d6309f00c
nbxx <SND\nbxx_cp>
2012-06-03 20:29:10 +0000

    add Color Utilities

---
+++ main/product/codes/DevLib.Utilities/ColorUtilities.cs
@@ -0,0 +1,25 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ColorUtilities.cs" company="YuGuan Corporation">
+//     Copyright (c) YuGuan Corporation. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace DevLib.Utilities
+{
+    using System;
+    using System.Drawing;
+
+    /// <summary>
+    /// Color Utilities
+    /// </summary>
+    public static class ColorUtilities
+    {
+        /// <summary>
+        /// Returns a random color
+        /// </summary>
+        /// <returns>The result color</returns>
+        public static Color GetRandomColor()
+        {
+            return Color.FromKnownColor((KnownColor)new Random().Next(1, 174));
+        }
+    }
+}

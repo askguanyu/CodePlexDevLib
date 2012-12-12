7cf405b948880631262ec915b64915d5d38a3850
nbxx <SND\nbxx_cp>
2012-12-12 15:18:28 +0000

    add Singleton Design Pattern

---
+++ main/product/codes/DevLib.DesignPatterns/Singleton.cs
@@ -0,0 +1,50 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Singleton.cs" company="YuGuan Corporation">
+//     Copyright (c) YuGuan Corporation. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace DevLib.DesignPatterns
+{
+    /// <summary>
+    /// Singleton Design Pattern
+    /// </summary>
+    public sealed class Singleton<T> where T : new()
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        private static readonly object _syncRoot = new object();
+
+        /// <summary>
+        ///
+        /// </summary>
+        private static T _instance;
+
+        /// <summary>
+        ///
+        /// </summary>
+        private Singleton() { }
+
+        /// <summary>
+        /// Gets singleton instance.
+        /// </summary>
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_syncRoot)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new T();
+                        }
+                    }
+                }
+
+                return _instance;
+            }
+        }
+    }
+}

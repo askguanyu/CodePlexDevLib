4cfc1a74ff368c9d5861c1645e8d2914182d774a
nbxx <SND\nbxx_cp>
2013-06-26 20:33:55 +0000

    1 add queue interface for ProducerConsumer Pattern

---
+++ main/product/codes/DevLib.DesignPatterns/IProducerConsumerQueue.cs
@@ -0,0 +1,52 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="IProducerConsumerQueue.cs" company="YuGuan Corporation">
+//     Copyright (c) YuGuan Corporation. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace DevLib.DesignPatterns
+{
+    /// <summary>
+    /// Represents a first-in, first-out collection of objects interface.
+    /// </summary>
+    /// <typeparam name="T">Specifies the type of elements in the queue.</typeparam>
+    public interface IProducerConsumerQueue<T>
+    {
+        /// <summary>
+        /// Gets the number of elements contained in the <see cref="IProducerConsumerQueue{T}" />.
+        /// </summary>
+        int Count
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Removes all objects from the <see cref="IProducerConsumerQueue{T}" />.
+        /// </summary>
+        void Clear();
+
+        /// <summary>
+        /// Determines whether an element is in the <see cref="IProducerConsumerQueue{T}" />.
+        /// </summary>
+        /// <param name="item">The object to locate in the <see cref="IProducerConsumerQueue{T}" />. The value can be null for reference types.</param>
+        /// <returns>true if <paramref name="item" /> is found in the <see cref="IProducerConsumerQueue{T}" />; otherwise, false.</returns>
+        bool Contains(T item);
+
+        /// <summary>
+        /// Removes and returns the object at the beginning of the <see cref="IProducerConsumerQueue{T}" />.
+        /// </summary>
+        /// <returns>The object that is removed from the beginning of the <see cref="IProducerConsumerQueue{T}" />.</returns>
+        T Dequeue();
+
+        /// <summary>
+        /// Adds an object to the end of the <see cref="IProducerConsumerQueue{T}" />.
+        /// </summary>
+        /// <param name="item">The object to add to the <see cref="IProducerConsumerQueue{T}" />. The value can be null for reference types.</param>
+        void Enqueue(T item);
+
+        /// <summary>
+        /// Returns the object at the beginning of the <see cref="IProducerConsumerQueue{T}" /> without removing it.
+        /// </summary>
+        /// <returns>The object at the beginning of the <see cref="IProducerConsumerQueue{T}" />.</returns>
+        T Peek();
+    }
+}
---
+++ main/product/codes/DevLib.DesignPatterns/ProducerConsumerQueue.cs
@@ -0,0 +1,74 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ProducerConsumerQueue.cs" company="YuGuan Corporation">
+//     Copyright (c) YuGuan Corporation. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace DevLib.DesignPatterns
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Represents a first-in, first-out collection of objects for <see cref="ProducerConsumer{T}" />.
+    /// </summary>
+    /// <typeparam name="T">Specifies the type of elements in the queue.</typeparam>
+    public class ProducerConsumerQueue<T> : IProducerConsumerQueue<T>
+    {
+        /// <summary>
+        /// The queue that contains the data items.
+        /// </summary>
+        private readonly Queue<T> _queue = new Queue<T>();
+
+        /// <summary>
+        /// Gets the number of elements contained in the <see cref="ProducerConsumerQueue{T}" />.
+        /// </summary>
+        public int Count
+        {
+            get { return this._queue.Count; }
+        }
+
+        /// <summary>
+        /// Removes all objects from the <see cref="ProducerConsumerQueue{T}" />.
+        /// </summary>
+        public void Clear()
+        {
+            this._queue.Clear();
+        }
+
+        /// <summary>
+        /// Determines whether an element is in the <see cref="ProducerConsumerQueue{T}" />.
+        /// </summary>
+        /// <param name="item">The object to locate in the <see cref="ProducerConsumerQueue{T}" />. The value can be null for reference types.</param>
+        /// <returns>true if <paramref name="item" /> is found in the <see cref="ProducerConsumerQueue{T}" />; otherwise, false.</returns>
+        public bool Contains(T item)
+        {
+            return this._queue.Contains(item);
+        }
+
+        /// <summary>
+        /// Removes and returns the object at the beginning of the <see cref="ProducerConsumerQueue{T}" />.
+        /// </summary>
+        /// <returns>The object that is removed from the beginning of the <see cref="ProducerConsumerQueue{T}" />.</returns>
+        public T Dequeue()
+        {
+            return this._queue.Dequeue();
+        }
+
+        /// <summary>
+        /// Adds an object to the end of the <see cref="ProducerConsumerQueue{T}" />.
+        /// </summary>
+        /// <param name="item">The object to add to the <see cref="ProducerConsumerQueue{T}" />. The value can be null for reference types.</param>
+        public void Enqueue(T item)
+        {
+            this._queue.Enqueue(item);
+        }
+
+        /// <summary>
+        /// Returns the object at the beginning of the <see cref="ProducerConsumerQueue{T}" /> without removing it.
+        /// </summary>
+        /// <returns>The object at the beginning of the <see cref="ProducerConsumerQueue{T}" />.</returns>
+        public T Peek()
+        {
+            return this._queue.Peek();
+        }
+    }
+}

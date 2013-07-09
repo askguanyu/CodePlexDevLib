567e49e99ea973d509e7cdae09b52bae2d8163c4
nbxx <SND\nbxx_cp>
2013-07-09 20:28:56 +0000

    1 [version] [update] 1.6 2 [feature] [add] add zip file handling feature

---
+++ main/product/codes/DevLib.Compression/ZipArchiveMode.cs
@@ -0,0 +1,17 @@
+//-----------------------------------------------------------------------
+// <copyright file="ZipArchiveMode.cs" company="YuGuan Corporation">
+//     Copyright (c) YuGuan Corporation. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace DevLib.Compression
+{
+    /// <summary>
+    /// Specifies values for interacting with zip archive entries.
+    /// </summary>
+    public enum ZipArchiveMode
+    {
+        Read,
+        Create,
+        Update,
+    }
+}

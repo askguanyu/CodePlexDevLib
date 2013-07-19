d3a8b33d7f702459c66c1087b4d48f4c4accc228
nbxx <SND\nbxx_cp>
2013-07-19 04:28:52 +0000

    1 [feature] [add] add start service immediately or not property when install is done to DevLib.ServiceProcess

--- main/product/codes/DevLib.ServiceProcess/WindowsServiceSetup.cs
+++ main/product/codes/DevLib.ServiceProcess/WindowsServiceSetup.cs
@@ -118,6 +118,15 @@ namespace DevLib.ServiceProcess
         }
 
         /// <summary>
+        /// Gets or sets a value indicating whether to start service immediately after installed.
+        /// </summary>
+        public bool StartAfterInstall
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
         /// Gets or sets a value indicating whether to report Start, Stop, Pause, and Continue commands in the event log.
         /// </summary>
         public bool AutoLog
@@ -230,6 +239,7 @@ namespace DevLib.ServiceProcess
             }
 
             this.StartType = ServiceStartMode.Automatic;
+            this.StartAfterInstall = true;
             this.AutoLog = true;
             this.CanHandlePowerEvent = true;
             this.CanHandleSessionChangeEvent = true;

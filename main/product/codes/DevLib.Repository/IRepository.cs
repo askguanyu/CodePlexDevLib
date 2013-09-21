fe71457e11dba5013f5a73e18ffa70453fcf795c
nbxx <SND\nbxx_cp>
2013-09-21 03:50:26 +0000

    1 [version] [update] 1.11 2 [project] [add] DevLib.Repository

---
+++ main/product/codes/DevLib.Repository/IRepository.cs
@@ -0,0 +1,48 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="IRepository.cs" company="YuGuan Corporation">
+//     Copyright (c) YuGuan Corporation. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace DevLib.Repository
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Generic repository interface for reading and writing domain entities to a storage.
+    /// </summary>
+    /// <typeparam name="TEntity">Type of entity.</typeparam>
+    /// <typeparam name="TPrimaryKey">Type of the primary key.</typeparam>
+    public interface IRepository<TEntity, TPrimaryKey> where TEntity : class
+    {
+        /// <summary>
+        /// Inserts entity to the storage.
+        /// </summary>
+        /// <param name="entity">Entity instance.</param>
+        void Insert(TEntity entity);
+
+        /// <summary>
+        /// Updates entity in the storage.
+        /// </summary>
+        /// <param name="entity">Entity instance.</param>
+        void Update(TEntity entity);
+
+        /// <summary>
+        /// Deletes entity in the storage.
+        /// </summary>
+        /// <param name="entity">Entity instance.</param>
+        void Delete(TEntity entity);
+
+        /// <summary>
+        /// Gets entity from the storage by entity id.
+        /// </summary>
+        /// <param name="id">Entity id.</param>
+        /// <returns>Entity instance.</returns>
+        TEntity GetById(TPrimaryKey id);
+
+        /// <summary>
+        /// Gets all entities of the type from the storage.
+        /// </summary>
+        /// <returns>List of entity instance.</returns>
+        IList<TEntity> GetAll();
+    }
+}

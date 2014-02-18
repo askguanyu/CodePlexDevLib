3874e86760abb6ed7db01983fbd54f08a7cc59dc
nbxx <SND\nbxx_cp>
2014-02-18 16:50:03 +0000

    1 [CHG] update version to 1.18 2 [NEW] add new project DevLib.Expressions

---
+++ main/product/codes/DevLib.Expressions/Evaluator.cs
@@ -0,0 +1,47 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Evaluator.cs" company="YuGuan Corporation">
+//     Copyright (c) YuGuan Corporation. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace DevLib.Expressions
+{
+    using System;
+    using System.Linq.Expressions;
+
+    /// <summary>
+    /// Class Evaluator to perform partial evaluation of an expression tree.
+    /// </summary>
+    public static class Evaluator
+    {
+        /// <summary>
+        /// Performs evaluation and replacement of independent sub-trees.
+        /// </summary>
+        /// <param name="expression">The root of the expression tree.</param>
+        /// <param name="fnCanBeEvaluated">A function that decides whether a given expression node can be part of the local function.</param>
+        /// <returns>A new tree with sub-trees evaluated and replaced.</returns>
+        public static Expression PartialEval(this Expression expression, Func<Expression, bool> fnCanBeEvaluated)
+        {
+            return new SubtreeEvaluator(new Nominator(fnCanBeEvaluated).Nominate(expression)).Eval(expression);
+        }
+
+        /// <summary>
+        /// Performs evaluation and replacement of independent sub-trees.
+        /// </summary>
+        /// <param name="expression">The root of the expression tree.</param>
+        /// <returns>A new tree with sub-trees evaluated and replaced.</returns>
+        public static Expression PartialEval(this Expression expression)
+        {
+            return PartialEval(expression, Evaluator.CanBeEvaluatedLocally);
+        }
+
+        /// <summary>
+        /// Method CanBeEvaluatedLocally.
+        /// </summary>
+        /// <param name="expression">The expression to check.</param>
+        /// <returns>true if the expression can be evaluated locally; otherwise, false.</returns>
+        private static bool CanBeEvaluatedLocally(Expression expression)
+        {
+            return expression.NodeType != ExpressionType.Parameter;
+        }
+    }
+}

3ae8daa4eef73f5872bc0304eb7832493cfb140d
nbxx <SND\nbxx_cp>
2014-09-11 21:47:51 +0000

    1 [CHG] fix StyleCop violation

--- main/product/codes/DevLib.Compression/ZipArchiveMode.cs
+++ main/product/codes/DevLib.Compression/ZipArchiveMode.cs
@@ -10,8 +10,19 @@ namespace DevLib.Compression
     /// </summary>
     public enum ZipArchiveMode
     {
+        /// <summary>
+        /// Only reading archive entries is permitted.
+        /// </summary>
         Read,
+
+        /// <summary>
+        /// Only creating new archive entries is permitted.
+        /// </summary>
         Create,
+
+        /// <summary>
+        /// Both read and write operations are permitted for archive entries.
+        /// </summary>
         Update,
     }
 }

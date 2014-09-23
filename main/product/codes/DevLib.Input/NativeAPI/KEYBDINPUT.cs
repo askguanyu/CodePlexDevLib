5a600af91d752ea10249530ec1434f5c1a6a3bfb
nbxx <SND\nbxx_cp>
2014-09-23 19:26:48 +0000

    1 [VER] 1.22.0 2 [NEW] add MemorySnapshot to detect memory leaking 3 [NEW] add DevLib.Input for simulation keyboard and mouse input 4 [NEW] add DevLib.Options for parsing command line arguments

---
+++ main/product/codes/DevLib.Input/NativeAPI/KEYBDINPUT.cs
@@ -0,0 +1,20 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="KEYBDINPUT.cs" company="YuGuan Corporation">
+//     Copyright (c) YuGuan Corporation. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace DevLib.Input.NativeAPI
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct KEYBDINPUT
+    {
+        public ushort wVk;
+        public ushort wScan;
+        public uint dwFlags;
+        public uint time;
+        public IntPtr dwExtraInfo;
+    }
+}

aa3026fe878bc21668403fe21ec529f83de7632b
nbxx <SND\nbxx_cp>
2014-12-18 04:06:18 +0000

    1 [VER] 1.28.0 2 [NEW] add new project DevLib.Logging.Viewer

---
+++ main/product/Codes/DevLib.Logging.Viewer/Program.cs
@@ -0,0 +1,27 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="YuGuan Corporation">
+//     Copyright (c) YuGuan Corporation. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace DevLib.Logging.Viewer
+{
+    using System;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// The main entry class.
+    /// </summary>
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}

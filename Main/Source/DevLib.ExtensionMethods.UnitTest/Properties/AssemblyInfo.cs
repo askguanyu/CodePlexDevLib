bcb5d76d20ecdf0ad383b9cc68658b8c31280be6
nbxx <SND\nbxx_cp>
2015-09-17 23:31:09 +0000

    1 [VER] 2.0.2 2 [NEW] add unit test

---
+++ Main/Source/DevLib.ExtensionMethods.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="YuGuan Corporation">
+//     Copyright (c) YuGuan Corporation. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DevLib.ExtensionMethods.UnitTest")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6df6676e-c652-48fc-97d2-94d3d077cb75")]

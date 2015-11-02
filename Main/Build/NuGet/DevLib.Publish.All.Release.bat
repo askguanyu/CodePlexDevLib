e7469b44157731aeef8468092666ba21721b148e
nbxx <SND\nbxx_cp>
2015-11-02 20:46:57 +0000

    1 [CHG] update nuget publish release build batch file

--- Main/Build/NuGet/DevLib.Publish.All.Release.bat
+++ Main/Build/NuGet/DevLib.Publish.All.Release.bat
@@ -1,40 +1,40 @@
 @echo off
 
-call DevLib.Addin.Publish.Release.bat
-call DevLib.Compression.Publish.Release.bat
-call DevLib.Configuration.Publish.Release.bat
-call DevLib.Csv.Publish.Release.bat
-call DevLib.DaemonProcess.Publish.Release.bat
-call DevLib.Data.Publish.Release.bat
-call DevLib.DesignPatterns.Publish.Release.bat
-call DevLib.Diagnostics.Publish.Release.bat
-call DevLib.DirectoryServices.Publish.Release.bat
-call DevLib.Drawing.Publish.Release.bat
-call DevLib.Dynamic.Publish.Release.bat
-call DevLib.Expressions.Publish.Release.bat
-call DevLib.ExtensionMethods.Publish.Release.bat
-call DevLib.Input.Publish.Release.bat
-call DevLib.IO.Publish.Release.bat
-call DevLib.Ioc.Publish.Release.bat
-call DevLib.Logging.Publish.Release.bat
-call DevLib.Logging.Viewer.Publish.Release.bat
-call DevLib.Management.Publish.Release.bat
-call DevLib.Metadata.Publish.Release.bat
-call DevLib.ModernUI.Publish.Release.bat
-call DevLib.Net.Publish.Release.bat
-call DevLib.Parameters.Publish.Release.bat
-call DevLib.Reflection.Publish.Release.bat
-call DevLib.Remoting.Publish.Release.bat
-call DevLib.Serialization.Publish.Release.bat
-call DevLib.ServiceModel.Publish.Release.bat
-call DevLib.ServiceModel.Extensions.Publish.Release.bat
-call DevLib.ServiceProcess.Publish.Release.bat
-call DevLib.TerminalServices.Publish.Release.bat
-call DevLib.Text.Publish.Release.bat
-call DevLib.Threading.Publish.Release.bat
-call DevLib.Timers.Publish.Release.bat
-call DevLib.Web.Publish.Release.bat
-call DevLib.Web.Hosting.WebHost20.Publish.Release.bat
-call DevLib.Web.Hosting.WebHost40.Publish.Release.bat
-call DevLib.WinForms.Publish.Release.bat
-call DevLib.Xml.Publish.Release.bat
+echo y | DevLib.Addin.Publish.Release.bat
+echo y | DevLib.Compression.Publish.Release.bat
+echo y | DevLib.Configuration.Publish.Release.bat
+echo y | DevLib.Csv.Publish.Release.bat
+echo y | DevLib.DaemonProcess.Publish.Release.bat
+echo y | DevLib.Data.Publish.Release.bat
+echo y | DevLib.DesignPatterns.Publish.Release.bat
+echo y | DevLib.Diagnostics.Publish.Release.bat
+echo y | DevLib.DirectoryServices.Publish.Release.bat
+echo y | DevLib.Drawing.Publish.Release.bat
+echo y | DevLib.Dynamic.Publish.Release.bat
+echo y | DevLib.Expressions.Publish.Release.bat
+echo y | DevLib.ExtensionMethods.Publish.Release.bat
+echo y | DevLib.Input.Publish.Release.bat
+echo y | DevLib.IO.Publish.Release.bat
+echo y | DevLib.Ioc.Publish.Release.bat
+echo y | DevLib.Logging.Publish.Release.bat
+echo y | DevLib.Logging.Viewer.Publish.Release.bat
+echo y | DevLib.Management.Publish.Release.bat
+echo y | DevLib.Metadata.Publish.Release.bat
+echo y | DevLib.ModernUI.Publish.Release.bat
+echo y | DevLib.Net.Publish.Release.bat
+echo y | DevLib.Parameters.Publish.Release.bat
+echo y | DevLib.Reflection.Publish.Release.bat
+echo y | DevLib.Remoting.Publish.Release.bat
+echo y | DevLib.Serialization.Publish.Release.bat
+echo y | DevLib.ServiceModel.Publish.Release.bat
+echo y | DevLib.ServiceModel.Extensions.Publish.Release.bat
+echo y | DevLib.ServiceProcess.Publish.Release.bat
+echo y | DevLib.TerminalServices.Publish.Release.bat
+echo y | DevLib.Text.Publish.Release.bat
+echo y | DevLib.Threading.Publish.Release.bat
+echo y | DevLib.Timers.Publish.Release.bat
+echo y | DevLib.Web.Publish.Release.bat
+echo y | DevLib.Web.Hosting.WebHost20.Publish.Release.bat
+echo y | DevLib.Web.Hosting.WebHost40.Publish.Release.bat
+echo y | DevLib.WinForms.Publish.Release.bat
+echo y | DevLib.Xml.Publish.Release.bat

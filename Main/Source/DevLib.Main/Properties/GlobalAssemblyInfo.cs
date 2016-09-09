d3e4c47180e96e7be9a819242d40f139ea5d4dab
nbxx <SND\nbxx_cp>
2016-09-09 01:15:58 +0000

    1 [VER] 2.7.33 2 [CHG] modify DownloadData to support proxy

--- Main/Source/DevLib.Main/Properties/GlobalAssemblyInfo.cs
+++ Main/Source/DevLib.Main/Properties/GlobalAssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Reflection;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.7.32.*")]
+[assembly: AssemblyVersion("2.7.33.*")]
 ////[assembly: AssemblyFileVersion("1.0.0.0")]

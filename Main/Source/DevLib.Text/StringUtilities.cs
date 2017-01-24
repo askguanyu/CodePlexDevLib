06110501880c12c8e520076e601f6c869ea0ace2
nbxx <SND\nbxx_cp>
2017-01-24 19:56:03 +0000

    1 [VER] 2.15.3 2 [NEW] add shorten string method 3 [BUG] fix bugs in json serializer in DevLib.Web

--- Main/Source/DevLib.Text/StringUtilities.cs
+++ Main/Source/DevLib.Text/StringUtilities.cs
@@ -7,6 +7,7 @@ namespace DevLib.Text
 {
     using System;
     using System.Text;
+    using System.Web.Security;
 
     /// <summary>
     /// String Utilities.
@@ -16,7 +17,23 @@ namespace DevLib.Text
         /// <summary>
         /// Static Field _random.
         /// </summary>
-        private static Random _random = new Random();
+        private static readonly Random RandomObj = new Random();
+
+        /// <summary>
+        /// The alphabet and numeric chars.
+        /// </summary>
+        private static readonly string[] AlphaNumericChars = new string[]
+        {
+            "a", "b", "c", "d", "e", "f", "g",
+            "h", "i", "j", "k", "l", "m", "n",
+            "o", "p", "q", "r", "s", "t",
+            "u", "v", "w", "x", "y", "z",
+            "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+            "A", "B", "C", "D", "E", "F", "G",
+            "H", "I", "J", "K", "L", "M", "N",
+            "O", "P", "Q", "R", "S", "T",
+            "U", "V", "W", "X", "Y", "Z"
+        };
 
         /// <summary>
         /// Returns a random alphabet string of the desired size.
@@ -34,7 +51,7 @@ namespace DevLib.Text
 
             for (int i = 0; i < size; i++)
             {
-                stringBuilder.Append(Convert.ToChar(Convert.ToInt32(Math.Floor((26 * _random.NextDouble()) + 65))));
+                stringBuilder.Append(Convert.ToChar(Convert.ToInt32(Math.Floor((26 * RandomObj.NextDouble()) + 65))));
             }
 
             return stringBuilder.ToString();
@@ -68,10 +85,45 @@ namespace DevLib.Text
 
             for (int i = 0; i < size; i++)
             {
-                stringBuilder.Append(charset[_random.Next(0, charsetBound)]);
+                stringBuilder.Append(charset[RandomObj.Next(0, charsetBound)]);
             }
 
             return stringBuilder.ToString();
+        }
+
+        /// <summary>
+        /// Shortens the specified source string.
+        /// </summary>
+        /// <param name="source">The source.</param>
+        /// <returns>4 shorten string candidates in string array.</returns>
+        public static string[] Shorten(string source)
+        {
+            if (string.IsNullOrEmpty(source))
+            {
+                return new[] { source, source, source, source };
+            }
+
+            string hex = FormsAuthentication.HashPasswordForStoringInConfigFile(source, "md5");
+
+            string[] result = new string[4];
+
+            for (int i = 0; i < 4; i++)
+            {
+                int hexInt = 0x3FFFFFFF & Convert.ToInt32("0x" + hex.Substring(i * 8, 8), 16);
+
+                string outChars = string.Empty;
+
+                for (int j = 0; j < 6; j++)
+                {
+                    int index = 0x0000003D & hexInt;
+                    outChars += AlphaNumericChars[index];
+                    hexInt = hexInt >> 5;
+                }
+
+                result[i] = outChars;
+            }
+
+            return result;
         }
     }
 }
---
+++ Main/Source/DevLib.Web/WebUtilities.cs
@@ -0,0 +1,16 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="WebUtilities.cs" company="YuGuan Corporation">
+//     Copyright (c) YuGuan Corporation. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace DevLib.Web
+{
+    using System;
+
+    /// <summary>
+    /// Web Utilities Class.
+    /// </summary>
+    public static class WebUtilities
+    {
+    }
+}

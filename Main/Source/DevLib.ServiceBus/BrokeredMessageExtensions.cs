1dba74351560afe94a3797413923a0b4113170db
nbxx <SND\nbxx_cp>
2017-02-10 22:44:57 +0000

    1 [VER] 2.16.3; 2 [CHG] add extension method for PipeMessage; 3 [CHG] add extension method for BrokeredMessage;

---
+++ Main/Source/DevLib.ServiceBus/BrokeredMessageExtensions.cs
@@ -0,0 +1,60 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="BrokeredMessageExtensions.cs" company="YuGuan Corporation">
+//     Copyright (c) YuGuan Corporation. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace DevLib.ServiceBus
+{
+    /// <summary>
+    /// BrokeredMessage extension methods.
+    /// </summary>
+    public static class BrokeredMessageExtensions
+    {
+        /// <summary>
+        /// Sets source object to the brokered message body.
+        /// </summary>
+        /// <param name="source">The source object.</param>
+        /// <param name="message">The brokered message.</param>
+        /// <returns>The BrokeredMessage instance.</returns>
+        public static BrokeredMessage ToBrokeredMessage(this object source, BrokeredMessage message = null)
+        {
+            if (message != null)
+            {
+                return message.SetBody(source);
+            }
+
+            return new BrokeredMessage(source);
+        }
+
+        /// <summary>
+        /// Gets the object from the brokered message body.
+        /// </summary>
+        /// <param name="source">The source BrokeredMessage.</param>
+        /// <returns>The object from the brokered message body.</returns>
+        public static object ToObject(this BrokeredMessage source)
+        {
+            if (source == null)
+            {
+                return null;
+            }
+
+            return source.GetBody();
+        }
+
+        /// <summary>
+        /// Gets the object from the brokered message body.
+        /// </summary>
+        /// <typeparam name="T">The type to which the message body will be deserialized.</typeparam>
+        /// <param name="source">The source BrokeredMessage.</param>
+        /// <returns>The object from the brokered message body.</returns>
+        public static T ToObject<T>(this BrokeredMessage source)
+        {
+            if (source == null)
+            {
+                return default(T);
+            }
+
+            return source.GetBody<T>();
+        }
+    }
+}

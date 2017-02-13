77b20a441370551b142d97c02dec504ff6cf9e9f
nbxx <SND\nbxx_cp>
2017-02-13 23:51:00 +0000

    1 [CHG] add xml comment for ExtensionAttribute;

--- Main/Source/DevLib.Legacy.Linq/System/Runtime/CompilerServices/ExtensionAttribute.cs
+++ Main/Source/DevLib.Legacy.Linq/System/Runtime/CompilerServices/ExtensionAttribute.cs
@@ -1,5 +1,8 @@
 namespace System.Runtime.CompilerServices
 {
+    /// <summary>
+    /// Indicates that a method is an extension method, or that a class or assembly contains extension methods.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class | AttributeTargets.Assembly)]
     public sealed class ExtensionAttribute : Attribute
     {

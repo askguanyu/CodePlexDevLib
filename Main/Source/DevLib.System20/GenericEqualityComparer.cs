d3e5887940152271264d4f17a89765044455133f
nbxx <SND\nbxx_cp>
2017-05-09 21:54:00 +0000

    1 [VER] 2.17.0; 2 [NEW] add DevLib.System20 project;

---
+++ Main/Source/DevLib.System20/GenericEqualityComparer.cs
@@ -0,0 +1,152 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="GenericEqualityComparer.cs" company="YuGuan Corporation">
+//     Copyright (c) YuGuan Corporation. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace System
+{
+    using System.Collections;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Generic methods to support the comparison of objects for equality.
+    /// </summary>
+    /// <typeparam name="T">The type of objects to compare.</typeparam>
+    public class GenericEqualityComparer<T> : IEqualityComparer, IEqualityComparer<T>
+    {
+        /// <summary>
+        /// The key selectors.
+        /// </summary>
+        private readonly Converter<T, object>[] _keySelectors;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GenericEqualityComparer{T}"/> class.
+        /// </summary>
+        /// <param name="keySelectors">The key selectors.</param>
+        public GenericEqualityComparer(params Converter<T, object>[] keySelectors)
+        {
+            this._keySelectors = keySelectors;
+        }
+
+        /// <summary>
+        /// Gets a new instance of the <see cref="GenericEqualityComparer{T}"/> class.
+        /// </summary>
+        /// <param name="keySelectors">The key selectors.</param>
+        /// <returns>GenericEqualityComparer instance.</returns>
+        public static GenericEqualityComparer<T> By(params Converter<T, object>[] keySelectors)
+        {
+            return new GenericEqualityComparer<T>(keySelectors);
+        }
+
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <param name="obj">The <see cref="T:System.Object" /> for which a hash code is to be returned.</param>
+        /// <returns>A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table.</returns>
+        public int GetHashCode(object obj)
+        {
+            int result = 0;
+
+            if (obj != null)
+            {
+                foreach (var keySelector in this._keySelectors)
+                {
+                    result ^= keySelector((T)obj).GetHashCode();
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Determines whether the specified objects are equal.
+        /// </summary>
+        /// <param name="x">The first object to compare.</param>
+        /// <param name="y">The second object to compare.</param>
+        /// <returns>true if the specified objects are equal; otherwise, false.</returns>
+        public bool Equals(T x, T y)
+        {
+            if (x == null && y == null)
+            {
+                return true;
+            }
+            else if (x == null | y == null)
+            {
+                return false;
+            }
+            else if (this.AllKeysEquals(x, y))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <param name="obj">The <see cref="T:System.Object" /> for which a hash code is to be returned.</param>
+        /// <returns>A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table.</returns>
+        public int GetHashCode(T obj)
+        {
+            int result = 0;
+
+            if (obj != null)
+            {
+                foreach (var keySelector in this._keySelectors)
+                {
+                    result ^= keySelector(obj).GetHashCode();
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Determines whether the specified objects are equal.
+        /// </summary>
+        /// <param name="x">The first object to compare.</param>
+        /// <param name="y">The second object to compare.</param>
+        /// <returns>true if the specified objects are equal; otherwise, false.</returns>
+        bool IEqualityComparer.Equals(object x, object y)
+        {
+            if (x == null && y == null)
+            {
+                return true;
+            }
+            else if (x == null | y == null)
+            {
+                return false;
+            }
+            else if (this.AllKeysEquals((T)x, (T)y))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Determines whether all the specified keys are equal.
+        /// </summary>
+        /// <param name="x">The first object to compare.</param>
+        /// <param name="y">The second object to compare.</param>
+        /// <returns>true if all the specified keys are equal; otherwise, false.</returns>
+        private bool AllKeysEquals(T x, T y)
+        {
+            foreach (var keySelector in this._keySelectors)
+            {
+                if (!keySelector(x).Equals(keySelector(y)))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Main/Source/DevLib.System20/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="YuGuan Corporation">
+//     Copyright (c) YuGuan Corporation. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DevLib.System20")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e18a4cf2-6820-4696-92e5-2c2433c2675d")]
